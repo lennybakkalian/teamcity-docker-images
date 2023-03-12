56391a73b2c107ce7dd86d7793ab354b4a9394cb
Lenny Bakkalian <lenny.bakkalian@gmail.com>
2023-03-12 23:07:29 +0100

    custom agent_path

--- context/generated/teamcity-minimal-agent-EAP-linux-customdir.sh
+++ context/generated/teamcity-minimal-agent-EAP-linux-customdir.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-if [ "$#" -lt 1 ]; then
-    echo "define agent_path"
+if [ "$#" -lt 2 ]; then
+    echo "define agent_path and img name"
     exit 1
 fi
 
 echo "agent_path: $1"
+echo "img name: $2"
 
 
 cd ../..
@@ -14,8 +15,7 @@ echo TeamCity/webapps > context/.dockerignore
 echo TeamCity/devPackage >> context/.dockerignore
 echo TeamCity/lib >> context/.dockerignore
 
-#AGENT_PATH=/opt/buildagent
 AGENT_PATH=$1
-AGENT_IMG_NAME=tc-minimal-agent0
+AGENT_IMG_NAME=$2
 
 docker build --build-arg agent_path=$AGENT_PATH -f "context/generated/linux/MinimalAgent/Ubuntu/20.04/Dockerfile_customdir" -t $AGENT_IMG_NAME "context"
